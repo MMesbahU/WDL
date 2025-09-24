abaf8e001106f5f64036d74e79905027fe8e0b99
MMesbahU <mdmesbah@gmail.com>
2025-09-24 22:36:08 +0000

    update

--- run_somalier/02_somalierRelate.sh
+++ run_somalier/02_somalierRelate.sh
@@ -5,7 +5,7 @@
 # awk 'NR==1{print "#family_id\tsample_id\tpaternal_id\tmaternal_id\tsex\tphenotype"};NR>1{print $2"\t"$3"\t-9\t-9\t2\t-9"}' sample_id_file.tsv > sample_id.relate.ped
 ##
 ## Run 
-# bash /path/to/somalier /path/to/sample_id.relate.ped /path/to/extracted_files /path/to/output/outprefix 1>>/path/to/log/somalier_relate.log 2>>/path/to/log/somalier_relate.err
+# bash /path/to/WDL/run_somalier/02_somalierRelate.sh /path/to/somalier /path/to/sample_id.relate.ped /path/to/extracted_files /path/to/output/outprefix 1>>/path/to/log/somalier_relate.log 2>>/path/to/log/somalier_relate.err
 
 #
 # https://github.com/brentp/somalier
